bb8f150a5114b09766238572a2b2c40e0e2b24f5
MohitSingh7979 <mohitsinghatwork@gmail.com>
2022-12-16 15:51:14 +0530

    java exp 2a file completed and added

---
+++ java_programming/exp_2/class_object_methods_arrays/CBMA.java
@@ -0,0 +1,11 @@
+class CBMA {
+
+    int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    public static void main(String[] args) {
+        CBMA n = new CBMA();
+        for (int v : n.arr) {
+            System.out.println(v);
+        }
+    }
+}
