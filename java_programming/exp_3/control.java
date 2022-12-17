4a1227361f530acd7f48089850966e08934c0e28
MohitSingh7979 <mohitsinghatwork@gmail.com>
2022-12-17 08:35:43 +0530

    exp 3 half done, userDefinedfunc remains

---
+++ java_programming/exp_3/control.java
@@ -0,0 +1,69 @@
+public class control {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 10;
+
+        if (i + j > 10) {
+            System.out.println("If Statement");
+        }
+
+        if (i > j) {
+        } else {
+            System.out.println("If else Statement");
+        }
+
+        if (i > j) {
+        } else if (j < i) {
+        } else {
+            System.out.println("If else if ladder Statement");
+        }
+
+        if (i < j) {
+            if (j > i) {
+                if (j + i > 10) {
+                    System.out.println("Nested If statement");
+                }
+            }
+        }
+
+        switch (j) {
+            case 0: {
+            }
+                break;
+            default:
+                System.out.println("Switch statement");
+                break;
+        }
+
+        for (; j < 11;) {
+            System.out.println("For loop statement");
+            j++;
+        }
+
+        String[] names = { "For", "each", "loop", "statement" };
+        for (String name : names) {
+            System.out.print(name + " ");
+        }
+
+        while (i > 0) {
+            System.out.println("\n" + "While loop statement");
+            i--;
+        }
+
+        do {
+            System.out.println("Do While loop statement");
+        } while (i < 0);
+
+        for (; i < j;) {
+            System.out.println("Break statement");
+            break;
+        }
+
+        for (; i < 1; i++) {
+            if (i == 2) {
+                continue;
+            }
+            System.out.println("Continue Statement");
+        }
+    }
+}
